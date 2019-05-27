2a0bc474769a6e8cfe6083add2b71cc533b0e47e
mraj <madhu.raj@viasat.com>
2019-05-27 05:48:28 +0000

    factorial practice exercise

---
+++ L1_Practice/factorial.py
@@ -0,0 +1,13 @@
+#Desrciption - Script to find factorial of a given set of numbers
+#Author - Madhu valan
+#Remarks - Practice
+myList = []
+sum = 1
+n = 5
+i = 1
+while(n>=i):
+  sum = sum * i 
+  print ("i.{} sum{}".format(i,sum))
+  i+=1 
+print(sum)
+
